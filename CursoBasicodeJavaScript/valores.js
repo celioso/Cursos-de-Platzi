1b56edfd7eff323e3909e4c3285c1fcbb9c6757a
Mario Celis <celioso1@hotmail.com>
2023-04-24 21:09:43 -0500

    curso de javaScript Primer dia

---
+++ CursoBasicodeJavaScript/valores.js
@@ -0,0 +1,17 @@
+//valores
+40 
+ 
+// cadena de texto 
+"Diego De Granda" // cadena de texto 
+
+//Booleans
+true
+false 
+
+//play honders
+null
+undefined
+
+//valor tipo objeto
+[1,2,3]
+{nombre:"Mario"}
---
+++ CursoBasicodeJavaScript/variables.js
@@ -0,0 +1,19 @@
+var nombre = "Mario";
+
+// declarar
+var edad;
+
+edad
+// Inicializar
+edad = 30;
+
+edad 
+
+var elementos = ["computadora","celular"];
+
+var persona = {
+    nombre:"Diago",
+    edad: 30
+}
+
+persona;
