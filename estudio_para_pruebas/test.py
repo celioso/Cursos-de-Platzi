f5fcee4f60bc2838a36073e89b72874588687b9d
Mario Celis <celioso1@hotmail.com>
2024-01-15 21:12:26 -0500

    add test 15/01/2024

--- estudio_para_pruebas/test.py
+++ estudio_para_pruebas/test.py
@@ -1,1 +1,13 @@
-print("hola")
+def datos (self,name, years):
+    self.name=name
+    self.years = years
+    name = input("cual es su nombre ")
+    years = input("ingrese su edad ")
+
+
+print("Hola, bienvenido a nuestra compañia")
+print(f"Su nombre es {name} y tiene {years}")
+
+if __name__=="__main__":
+    print("Hola, bienvenido a nuestra compañia")
+    datos()
