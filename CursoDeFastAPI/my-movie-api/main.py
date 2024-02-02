b0fdbdce1441d3425f04f80fe6576b0299a20d73
Mario Celis <celioso1@hotmail.com>
2024-02-01 21:38:13 -0500

    01/02/2024

--- CursoDeFastAPI/my-movie-api/main.py
+++ CursoDeFastAPI/my-movie-api/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 app = FastAPI()
@@ -9,12 +9,24 @@ app.version = "0.0.1"
 
 class Movie(BaseModel):
     id:Optional[int] = None
-    title:str
-    overview: str
-    year:int
-    rating:float
-    category: str
-    
+    title:str = Field(min_length=5, max_length=15)
+    overview: str = Field(min_length=15, max_length=50)
+    year:int  = Field(le = 2022)
+    rating:float = Field(default=0, le=10)
+    category: str = Field(min_length=4, max_length=20)
+    class Config:
+        json_schema_extra = {
+            "example": [
+                {
+                "id":1,
+                "title": "Mi película",
+                "overview":"Descripción de la película",
+                "year": 2022,
+                "rating":9.8,
+                "category": "Acción"
+			}
+            ]
+		}
     
 
 movies = [
