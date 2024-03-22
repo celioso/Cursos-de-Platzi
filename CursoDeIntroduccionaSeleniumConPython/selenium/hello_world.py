e409f7a579990e5565c981ef2357d158ed7894d1
Mario Celis <celioso1@hotmail.com>
2024-03-21 19:44:31 -0500

    add hello_world.py

--- CursoDeIntroduccionaSeleniumConPython/selenium/hello_world.py
+++ CursoDeIntroduccionaSeleniumConPython/selenium/hello_world.py
@@ -1,17 +1,20 @@
 import unittest
-from pyunitreport import HTMLtestRunner
+from pyunitreport import HTMLTestRunner
 from selenium import webdriver
 
 class HelloWorld(unittest.TestCase):
 
     def setUp(self) -> None:
-        return super().setUp()
+        self.driver = webdriver.Chrome(executable_path = r"chromedriver.exe")
+        driver = self.driver
+        driver.implicitly_wait(10)
     
     def test_hello_world(self):
-        pass
+        driver = self.driver
+        driver.get("https://www.platzi.com")
 
     def tearDown(self):
-        return super().tearDown()
+        self.driver.quit()
     
 if __name__ == "__main__":
-    unittest.main(verbosity=2, testRunner=HTMLtestRunner(output="reportes", report_name = "hello-world-report"))
+    unittest.main(verbosity=2, testRunner=HTMLTestRunner(output="reportes", report_name = "hello-world-report"))
