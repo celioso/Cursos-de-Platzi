74862d294bc42c81b39feeb3a906dbea6bed18ad
Mario Celis <celioso1@hotmail.com>
2024-03-23 20:29:58 -0500

    add hello_world.py

--- CursoDeIntroduccionaSeleniumConPython/selenium/hello_world.py
+++ CursoDeIntroduccionaSeleniumConPython/selenium/hello_world.py
@@ -1,21 +1,20 @@
-import unittest 
+import unittest
 from pyunitreport import HTMLTestRunner
-from selenium import webdriver 
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 
 class HelloWorld(unittest.TestCase):
-    def setUp(self) -> None:
-        options = Options()
-        self.driver = webdriver.Chrome(service=Service(r"C:\Users\celio\OneDrive\Escritorio\programación\platzi\CursoDeIntroduccionaSeleniumConPython\selenium\chromedriver.exe"), options=options)
-        self.driver.implicitly_wait(10)
-    
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(chrome_driver_path = r"C:\Users\celio\OneDrive\Escritorio\programación\platzi\CursoDeIntroduccionaSeleniumConPython\selenium\chromedriver.exe")
+        driver = self.driver
+        driver.implicitly_wait(10)
+
     def test_hello_world(self):
         driver = self.driver
-        driver.get("https://www.platzi.com")
+        driver.get('https://www.platzi.com')
 
     def tearDown(self):
         self.driver.quit()
 
-if __name__ == '__main__':
-    unittest.main(verbosity = 2, testRunner = HTMLTestRunner(output = 'Reports', report_name = 'Hello_world_report'))
+if __name__ == "__main__":
+    unittest.main(verbosity = 2, testRunner = HTMLTestRunner(output= 'reportes',report_name= 'helllo_world_report'))
--- CursoDeIntroduccionaSeleniumConPython/selenium/test.py
+++ CursoDeIntroduccionaSeleniumConPython/selenium/test.py
@@ -1,4 +1,31 @@
+import unittest
+from pyunitreport import HTMLTestRunner
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-browser = webdriver.Firefox()
-browser.get('http://selenium.dev/')
+
+class HelloWorld(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver = cls.driver
+        driver.implicitly_wait(20)
+
+    # aquí al medio van los tests
+    def test_hello_world(self):
+        driver = self.driver
+        driver.get("https://www.platzi.com")
+
+    def test_visit_wikipedia(self):
+        driver = self.driver
+        driver.get("https://www.wikipedia.org")
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main(testRunner=HTMLTestRunner(output="reportes", report_name="hello-world-report"), verbosity=2,)
