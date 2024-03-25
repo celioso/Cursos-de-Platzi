da8f43b03c59a76a47ff71b62fba3d23f4c2eb87
celioso <celioso1@hotmail.com>
2024-03-24 19:49:15 -0500

    add reports in Linux

--- CursoDeIntroduccionaSeleniumConPython/selenium/hello_world.py
+++ CursoDeIntroduccionaSeleniumConPython/selenium/hello_world.py
@@ -6,21 +6,24 @@ class HelloWorld(unittest.TestCase):
 
     @classmethod
     def setUp(cls):
-        chrome_driver_path = r'/chromedriver.exe'
+        chrome_driver_path = r"/chromedriver.exe"
         cls.driver = webdriver.Chrome()
         driver = cls.driver
-        driver.implicitly_wait(10)
+        driver.implicitly_wait(20)
 
     def test_hello_world(self):
         driver = self.driver
-        driver.get('https://www.platzi.com')
+        driver.get("https://www.platzi.com")
 
     def test_visit_wikipedia(self):
         self.driver.get("https://www.wikipedia.org")
+
+    def test_visit_google(self):
+        self.driver.get("https://www.google.com.co")
 
     @classmethod
     def tearDownClass(cls):
         cls.driver.quit()
         
 if __name__ == "__main__":
-    unittest.main(verbosity = 2, testRunner = HTMLTestRunner(output= 'reportes',report_name= 'helllo_world_report'))
+    unittest.main(verbosity = 2, testRunner = HTMLTestRunner(output= "reportes",report_name= "helllo_world_report"))
