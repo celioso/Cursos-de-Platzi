659468ec6f65cdfdc1e79e163c361c13196d1f1f
Mario Celis <celioso1@hotmail.com>
2024-09-30 22:24:39 -0500

    django

--- CursodeDjangoRestFramework/django-rest-framework/doctorapp/urls.py
+++ CursodeDjangoRestFramework/django-rest-framework/doctorapp/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from patients.views import list_patients
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/patients', list_patients)
 ]
